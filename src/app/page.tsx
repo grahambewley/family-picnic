f1b7573db71e50a0803fc2c1b0d3c11642fa897c
Graham Bewley <grahambewley@gmail.com>
2025-03-22 21:36:43 -0400

    Add banner to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import styles from "./page.module.css";
 import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
+import getConfig from "next/config";
 import CampDetailsCard from "./components/CampDetailsCard";
 import AboutCard from "./components/AboutCard";
 import RideDetailsCard from "./components/RideDetailsCard";
+import Link from "next/link";
+
+const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
+  const { nextRideDate, nextRideTime, nextRideLocation, nextRideStravaRouteUrl, nextRideInfo } = publicRuntimeConfig;
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -17,7 +24,15 @@ export default function Home() {
           <RideDetailsCard />
         </div>
       </main>
-
+      <div className={styles.banner}>
+        <span>You coming?</span>
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLSeLS29hnSCrZT4ufjm9Ju-TFGDftEyms2_RenQgvNddhHjZlA/viewform?usp=dialog"
+          className={styles.linkButton}
+        >
+          RSVP
+        </Link>
+      </div>
     </div>
   );
 }
